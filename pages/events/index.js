22749f98841247c14c678742825473dbc8cff3cd
robdan89 <fuquardwork@gmail.com>
2021-04-21 12:46:53 -0500

    added funcionality to to the events-search component. Submition of the search criteria pushes the router to [...slug] page.

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,15 +1,23 @@
+import { useRouter } from "next/router";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 
-const EventsPage = () => {
+const AllEventsPage = () => {
+  const router = useRouter();
+
   const events = getAllEvents();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
   return (
     <div>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </div>
   );
 };
 
-export default EventsPage;
+export default AllEventsPage;
