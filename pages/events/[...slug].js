9f8752b99a35ac428452308b2ab5b64a7d744976
robdan89 <fuquardwork@gmail.com>
2021-04-21 13:25:09 -0500

    added errer aler ui comonents to [eventId] and [...slug] pages

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,8 @@
 import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert";
 import { getFilteredEvents } from "../../dummy-data";
 
 const FilteredEventsPage = () => {
@@ -22,12 +26,19 @@ const FilteredEventsPage = () => {
   if (
     isNaN(numYear) ||
     isNaN(numMonth) ||
-    numYear > 30 ||
-    numYear < 21 ||
+    numYear > 2030 ||
+    numYear < 2021 ||
     numMonth < 1 ||
     numMonth > 12
   ) {
-    return <p>Invalid filter. Please adjust your values.</p>;
+    return (
+      <>
+        <ErrorAlert>Invalid filter. Please adjust your values.</ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    );
   }
 
   const filteredEvents = getFilteredEvents({
@@ -36,13 +47,23 @@ const FilteredEventsPage = () => {
   });
 
   if (!filteredEvents || filteredEvents.length === 0) {
-    return <p>No events found for the chosen filter.</p>;
+    return (
+      <>
+        <ErrorAlert>No events found for the chosen filter.</ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    );
   }
 
+  const date = new Date(numYear, numMonth - 1);
+
   return (
-    <div>
-      <h1>Filtered Events</h1>
-    </div>
+    <>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </>
   );
 };
 
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -3,6 +3,7 @@ import { getEventById } from "../../dummy-data";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
 
 const EventDetailPage = () => {
   const router = useRouter();
@@ -12,7 +13,7 @@ const EventDetailPage = () => {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No event found</p>;
+    return <ErrorAlert>No event found</ErrorAlert>;
   }
 
   return (
